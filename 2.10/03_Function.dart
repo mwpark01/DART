df665ec68a3216087ffe1ee2773c5f1830059ca8
mwpark01 <hyminwoo0000@gmail.com>
2025-02-11 00:38:17 +0900

    비동기 함수, Future 함수

--- 2.10/03_Function.dart
+++ 2.10/03_Function.dart
@@ -1,35 +1,35 @@
-void main() {
-  // main function
-  // 실습: 함수 선언 및 호출
-  // 함수 호출
-  getCurrentDateTime();
-  var hourDifference = -7;
-  getCurrentDateTimeWithDifference(hourDifference);
+// void main() {
+//   // main function
+//   // 실습: 함수 선언 및 호출
+//   // 함수 호출
+//   getCurrentDateTime();
+//   var hourDifference = -7;
+//   getCurrentDateTimeWithDifference(hourDifference);
 
-  printGreetingNamed();
-  printGreetingNamed(personName: "Rich");
-  printGreetingNamed(personName: "Mary", clientId: 001);
+//   printGreetingNamed();
+//   printGreetingNamed(personName: "Rich");
+//   printGreetingNamed(personName: "Mary", clientId: 001);
 
-  // 익명 함수
-  int value = 5;
+//   // 익명 함수
+//   int value = 5;
   
-  // 5. 화살표 함수
-  int ex1Squared(num1) => num1 * num1;
-  int ex1Cubed(num1) => num1 * num1 * num1;
+//   // 5. 화살표 함수
+//   int ex1Squared(num1) => num1 * num1;
+//   int ex1Cubed(num1) => num1 * num1 * num1;
   
-  // 6. 중괄호 일반 함수
-  int ex2Squared(num2) {
-    return num2 * num2;
-  }
-  int ex2Cubed(num2) {
-    return num2 * num2 * num2;
-  }
+//   // 6. 중괄호 일반 함수
+//   int ex2Squared(num2) {
+//     return num2 * num2;
+//   }
+//   int ex2Cubed(num2) {
+//     return num2 * num2 * num2;
+//   }
   
-  print('EX1-Squared: ${ex1Squared(value)}');
-  print('EX1-Cubed: ${ex1Cubed(value)}');
-  print('EX2-Squared: ${ex2Squared(value)}');
-  print('EX2-Cubed: ${ex2Cubed(value)}');
-}
+//   print('EX1-Squared: ${ex1Squared(value)}');
+//   print('EX1-Cubed: ${ex1Cubed(value)}');
+//   print('EX2-Squared: ${ex2Squared(value)}');
+//   print('EX2-Cubed: ${ex2Cubed(value)}');
+// }
 
 // 1. 함수 선언
 void getCurrentDateTime() {
@@ -61,4 +61,24 @@ DateTime getTimeDifference(int hourDifference) {
   DateTime timeNow = DateTime.now();
   DateTime timeDifference = timeNow.add(Duration(hours: hourDifference));
   return timeDifference;
+}
+
+void main() async {
+  // Future 반환 값이 있는 함수 호출
+  int myDelay = 5;
+  print('Hello');
+  var customDelay = await _customDelay(myDelay);
+  var customText = myDelay == customDelay ? 'Success' : 'Failed';
+  print('Its $customDelay - $customText');
+}
+
+// 함수 선언: Future 반환 값이 있는 함수
+Future<int> _customDelay(int delay) async {
+  try {
+    await Future.delayed(Duration(seconds: delay));
+    return delay;
+  } catch (e) {
+    print(e);
+    return -1;
+  }
 }
